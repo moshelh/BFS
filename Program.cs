f6a7f70fca8aa325aba18dc958af01a51f246eb0
Moshe Elhadad <44942115+moshelh@users.noreply.github.com>
2020-05-06 19:26:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFS
+{
+    public class Graph<T>
+    {
+        public Graph() { }
+        public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
+        {
+            foreach (var vertex in vertices)
+                AddVertex(vertex);
+
+            foreach (var edge in edges)
+                AddEdge(edge);
+        }
+
+        public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+        public void AddVertex(T vertex)
+        {
+            AdjacencyList[vertex] = new HashSet<T>();
+        }
+
+        public void AddEdge(Tuple<T, T> edge)
+        {
+            if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+            {
+                AdjacencyList[edge.Item1].Add(edge.Item2);
+                AdjacencyList[edge.Item2].Add(edge.Item1);
+            }
+        }
+    }
+    public class Algorithms
+    {
+        //The code return an IEnumerable<T>, which provides all the vertices that make up the shortest path to get from the starting vertex to the destination vertex. 
+        
+        public Func<T, IEnumerable<T>> ShortestPathFunction<T>(Graph<T> graph, T start)
+        {
+            var previous = new Dictionary<T, T>();
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (previous.ContainsKey(neighbor))
+                        continue;
+
+                    previous[neighbor] = vertex;
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            Func<T, IEnumerable<T>> shortestPath = v => {
+                var path = new List<T> { };
+
+                var current = v;
+                while (!current.Equals(start))
+                {
+                    path.Add(current);
+                    current = previous[current];
+                };
+
+                path.Add(start);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vertices = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var edges = new[]{Tuple.Create(1,2), Tuple.Create(1,3),
+                Tuple.Create(2,4), Tuple.Create(3,5), Tuple.Create(3,6),
+                Tuple.Create(4,7), Tuple.Create(5,7), Tuple.Create(5,8),
+                Tuple.Create(5,6), Tuple.Create(8,9), Tuple.Create(9,10)};
+
+            var graph = new Graph<int>(vertices, edges);
+            var algorithms = new Algorithms();
+
+            var startVertex = 1;
+            var shortestPath = algorithms.ShortestPathFunction(graph, startVertex);
+            foreach (var vertex in vertices)
+                Console.WriteLine("shortest path to {0,2}: {1}",
+                        vertex, string.Join(", ", shortestPath(vertex)));
+
+            // shortest path to  1: 1
+            // shortest path to  2: 1, 2
+            // shortest path to  3: 1, 3
+            // shortest path to  4: 1, 2, 4
+            // shortest path to  5: 1, 3, 5
+            //shortest path to  6: 1, 3, 6
+            // shortest path to  7: 1, 2, 4, 7
+            //shortest path to  8: 1, 3, 5, 8
+            //shortest path to  9: 1, 3, 5, 8, 9
+            //shortest path to 10: 1, 3, 5, 8, 9, 10
+        }
+    }
+    }
+
